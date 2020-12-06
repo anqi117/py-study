8aa11f759dbdef7a58de2cce4af9b0d126972218
Joseph Ryu <anqi117@gmail.com>
2020-12-06 21:14:29 +0900

    tcp server socket

---
+++ serverSocket.py
@@ -0,0 +1,19 @@
+# server recive data by tcp
+
+from socket import *
+
+# create server socket
+serverSocket = socket(AF_INET, SOCK_STREAM)
+
+# bind
+serverSocket.bind(("", 8899))
+
+# turn to listen socket
+serverSocket.listen(5)
+
+# accept
+clientSocket, clientInfo = serverSocket.accept()
+
+recvData = clientSocket.recv(1024)
+
+print("%s, %s"%(str(clientInfo), recvData)) 
